24ec63bb6342f9632f13787bab9dd02c063067b5
Jan Schoska <jan.schoska@gmail.com>
2024-09-15 09:14:10 +0200

    [Bug] Fixed custom test not showing in selectable list of active tests

--- src/utils/tests.mjs
+++ src/utils/tests.mjs
@@ -13,17 +13,29 @@ export class TestRegistrationError extends Error {}
  *
  * Should a test implementation with this name already exist, fail with an exception.
  * @param {*} testClass
+ * @param {string[]} types What type of test is registered? Active? Opposed? Resist? Followup?
  * @throws {TestRegistrationError} if a test implementation with the same name already exists
  */
-export function registerTest(testClass) {
+export function registerTest(testClass, types = ["activeTests"]) {
     console.debug("SR5 Workshop | Registering test", testClass);
 
+    // Register test in general registry so it can be called when executing the action.
     if (game.shadowrun5e.tests[testClass.name])
         throw new TestRegistrationError(
-            `Test ${testClass.name} already exists`,
+            `Test ${testClass.name} already exists`
         );
 
     game.shadowrun5e.tests[testClass.name] = testClass;
+
+    // Register test in specific test type registry so it can be shown in the action item sheet selection.
+    for (const testType of types) {
+        if (game.shadowrun5e[testType][testClass.name])
+            throw new TestRegistrationError(
+                `Test ${testClass.name} already exists as ${testType}`
+            );
+
+        game.shadowrun5e[testType][testClass.name] = testClass;
+    }
 }
 
 export function registerTests() {
@@ -34,11 +46,11 @@ export function registerTests() {
         registerTest(CustomTest);
     } catch (error) {
         ui.notifications.error(
-            "SR5 Workshop | Module failed to register test implementation with Shadowrun5e system. This makes the module incompatible until it is updated.",
+            "SR5 Workshop | Module failed to register test implementation with Shadowrun5e system. This makes the module incompatible until it is updated."
         );
         console.error(
             "SR5 Workshop | Module failed to register test implementation with Shadowrun5e system.",
-            error,
+            error
         );
     }
 }
